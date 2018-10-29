067a667294bdb576f67206ad19c9a90a93e30bae
Joseph Wilson <ioe416@gmail.com>
2018-10-28 20:40:35 -0400

    e

--- WageTracker/Form1.Designer.cs
+++ WageTracker/Form1.Designer.cs
@@ -28,13 +28,167 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle7 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle8 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle9 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle10 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle11 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle12 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.payDate = new System.Windows.Forms.DateTimePicker();
+            this.gross_textBox = new System.Windows.Forms.TextBox();
+            this.net_textBox = new System.Windows.Forms.TextBox();
+            this.add_button = new System.Windows.Forms.Button();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 40);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.Size = new System.Drawing.Size(538, 398);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // payDate
+            // 
+            this.payDate.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.payDate.Location = new System.Drawing.Point(12, 12);
+            this.payDate.Name = "payDate";
+            this.payDate.Size = new System.Drawing.Size(83, 20);
+            this.payDate.TabIndex = 1;
+            // 
+            // gross_textBox
+            // 
+            this.gross_textBox.Location = new System.Drawing.Point(101, 12);
+            this.gross_textBox.Name = "gross_textBox";
+            this.gross_textBox.Size = new System.Drawing.Size(67, 20);
+            this.gross_textBox.TabIndex = 2;
+            // 
+            // net_textBox
+            // 
+            this.net_textBox.Location = new System.Drawing.Point(174, 12);
+            this.net_textBox.Name = "net_textBox";
+            this.net_textBox.Size = new System.Drawing.Size(67, 20);
+            this.net_textBox.TabIndex = 3;
+            // 
+            // add_button
+            // 
+            this.add_button.Location = new System.Drawing.Point(247, 11);
+            this.add_button.Name = "add_button";
+            this.add_button.Size = new System.Drawing.Size(50, 23);
+            this.add_button.TabIndex = 4;
+            this.add_button.Text = "ADD";
+            this.add_button.UseVisualStyleBackColor = true;
+            this.add_button.Click += new System.EventHandler(this.Save_Button_Clicked);
+            // 
+            // Column1
+            // 
+            dataGridViewCellStyle7.Format = "d";
+            dataGridViewCellStyle7.NullValue = null;
+            this.Column1.DefaultCellStyle = dataGridViewCellStyle7;
+            this.Column1.HeaderText = "Payday";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            // 
+            // Column2
+            // 
+            dataGridViewCellStyle8.Format = "C2";
+            dataGridViewCellStyle8.NullValue = null;
+            this.Column2.DefaultCellStyle = dataGridViewCellStyle8;
+            this.Column2.HeaderText = "Gross Pay";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            // 
+            // Column3
+            // 
+            dataGridViewCellStyle9.Format = "C2";
+            dataGridViewCellStyle9.NullValue = null;
+            this.Column3.DefaultCellStyle = dataGridViewCellStyle9;
+            this.Column3.HeaderText = "Net Pay";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            // 
+            // Column4
+            // 
+            dataGridViewCellStyle10.Format = "C2";
+            dataGridViewCellStyle10.NullValue = null;
+            this.Column4.DefaultCellStyle = dataGridViewCellStyle10;
+            this.Column4.HeaderText = "Yearly Gross";
+            this.Column4.Name = "Column4";
+            this.Column4.ReadOnly = true;
+            // 
+            // Column5
+            // 
+            dataGridViewCellStyle11.Format = "C2";
+            dataGridViewCellStyle11.NullValue = null;
+            this.Column5.DefaultCellStyle = dataGridViewCellStyle11;
+            this.Column5.HeaderText = "Weekly Average";
+            this.Column5.Name = "Column5";
+            this.Column5.ReadOnly = true;
+            // 
+            // Column6
+            // 
+            dataGridViewCellStyle12.Format = "C2";
+            dataGridViewCellStyle12.NullValue = null;
+            this.Column6.DefaultCellStyle = dataGridViewCellStyle12;
+            this.Column6.HeaderText = "Projected Annual";
+            this.Column6.Name = "Column6";
+            this.Column6.ReadOnly = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(562, 450);
+            this.Controls.Add(this.add_button);
+            this.Controls.Add(this.net_textBox);
+            this.Controls.Add(this.gross_textBox);
+            this.Controls.Add(this.payDate);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DateTimePicker payDate;
+        private System.Windows.Forms.TextBox gross_textBox;
+        private System.Windows.Forms.TextBox net_textBox;
+        private System.Windows.Forms.Button add_button;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
     }
 }
 
--- WageTracker/Form1.cs
+++ WageTracker/Form1.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +14,70 @@ namespace WageTracker
 {
     public partial class Form1 : Form
     {
+        string conn_string = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Joe\Documents\WageTracker.mdf;Integrated Security = True; Connect Timeout = 30";
+
+        decimal sum = 0;
+        decimal average = 0;
+        decimal projected = 0;
+
         public Form1()
         {
             InitializeComponent();
         }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Save_Button_Clicked(object sender, EventArgs e)
+        {
+            Fill_DataGridView();
+        }
+
+
+        private void Fill_DataGridView()
+        {
+
+            if (dataGridView1.RowCount != 0)
+            {
+                //dataGridView1.ColumnCount = 6;
+                decimal prevSum = Convert.ToDecimal(dataGridView1.Rows[dataGridView1.RowCount - 1].Cells[3].Value.ToString());
+                sum = prevSum + Convert.ToDecimal(gross_textBox.Text.ToString());
+                average = sum / (dataGridView1.RowCount + 1);
+                MessageBox.Show((dataGridView1.RowCount - 1).ToString());
+                projected = (average * (52));
+
+                dataGridView1.Rows.Add(new String[]
+                {
+                    payDate.Value.ToShortDateString(),
+                    gross_textBox.Text,
+                    net_textBox.Text,
+                    sum.ToString(),
+                    average.ToString(),
+                    projected.ToString()
+                });
+            }
+            else if (dataGridView1.RowCount == 0)
+            {
+
+                dataGridView1.Rows.Add(new String[]
+                {
+                    payDate.Value.ToShortDateString(),
+                    gross_textBox.Text,
+                    net_textBox.Text,
+                    gross_textBox.Text,
+                    gross_textBox.Text,
+                    (Convert.ToDecimal(gross_textBox.Text.ToString())*52).ToString()
+                });
+            }
+            
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+
+        }
     }
 }
+
